456a6576ddf28de12d3edebdf679cb38a051304b
a178300 <oleksandr.chernookov@consultant.volvo.com>
2017-07-12 12:31:03 +0200

    added object initialization with fake top and bottom

--- src/main/java/Percolation.java
+++ src/main/java/Percolation.java
@@ -1,3 +1,5 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 /**
  * Created by a178300 on 7/10/2017.
  */
@@ -5,11 +7,18 @@ public class Percolation {
 
     private int gridSize;
     private boolean grid[][];
+    private WeightedQuickUnionUF wduf;
 
     // create n-by-n grid, with all sites blocked
     public Percolation(int n) {
         this.gridSize = n * n + 2;
-        grid = new boolean[n][n];
+        wduf = new WeightedQuickUnionUF(gridSize);
+        for (int i = 1; i <= n; i++) {
+            wduf.union(0, i);
+        }
+        for (int j = gridSize - 1; j >= gridSize - 1 - n; j--) {
+            wduf.union(gridSize - 1, j);
+        }
     }
 
     // open site (row, col) if it is not open already
@@ -37,6 +46,7 @@ public class Percolation {
     }
 
     public static void main(String[] args) {
-
+        Percolation p = new Percolation(5);
+        System.out.print("SSSSS");
     }
 }
