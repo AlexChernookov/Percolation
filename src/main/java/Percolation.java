2287e2df1241b2425fa9b6b8f42177248674d387
a178300 <oleksandr.chernookov@consultant.volvo.com>
2017-07-12 15:42:42 +0200

    no message

--- src/main/java/Percolation.java
+++ src/main/java/Percolation.java
@@ -6,28 +6,41 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class Percolation {
 
     private int gridSize;
-    private boolean grid[][];
-    private WeightedQuickUnionUF wduf;
+    private int N;
+    private boolean [][] grid;
+    private WeightedQuickUnionUF gridConnection;
 
     // create n-by-n grid, with all sites blocked
     public Percolation(int n) {
         this.gridSize = n * n + 2;
-        wduf = new WeightedQuickUnionUF(gridSize);
+        this.grid = new boolean[n][n];
+        gridConnection = new WeightedQuickUnionUF(gridSize);
         for (int i = 1; i <= n; i++) {
-            wduf.union(0, i);
-        }
-        for (int j = gridSize - 1; j >= gridSize - 1 - n; j--) {
-            wduf.union(gridSize - 1, j);
+            //connect top
+            gridConnection.union(0, i);
+            //connect bottom
+            gridConnection.union(n * n + 1, n * n - i + 1);
         }
     }
 
     // open site (row, col) if it is not open already
     public void open(int row, int col) {
+        grid[row-1][col-1] = true;
+    }
+
+    // get corresponding number to connect in gridConnection
+    private int getCorrespondingNumber(int row, int col){
+        return (row-1)*N+col;
+    }
+
+    // connect neighbors of element if they are opened
+    private void connectNeighbors(int row, int col){
+
     }
 
     // is site (row, col) open?
     public boolean isOpen(int row, int col) {
-        return false;
+        return grid[row][col];
     }
 
     // is site (row, col) full?
