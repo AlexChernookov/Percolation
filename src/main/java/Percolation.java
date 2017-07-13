c9a2f332b4489114aa0ac947d80bfac6272b9c33
a178300 <oleksandr.chernookov@consultant.volvo.com>
2017-07-13 15:38:14 +0200

    added check sides and connect them if open

--- src/main/java/Percolation.java
+++ src/main/java/Percolation.java
@@ -9,8 +9,8 @@ public class Percolation {
     private int N;
     private boolean[][] grid;
     private WeightedQuickUnionUF gridConnection;
-    private int top = 0;
-    private int bottom;
+    private int TOP = 0;
+    private int BOTTOM;
 
     // create n-by-n grid, with all sites blocked
     public Percolation(int n) {
@@ -18,18 +18,21 @@ public class Percolation {
         this.gridConnectionSize = n * n + 2;
         this.grid = new boolean[n][n];
         gridConnection = new WeightedQuickUnionUF(gridConnectionSize);
-        this.bottom = n * n + 1;
+        this.BOTTOM = n * n + 1;
         for (int i = 1; i <= n; i++) {
             //connect top
-            gridConnection.union(top, i);
+            gridConnection.union(TOP, i);
             //connect bottom
-            gridConnection.union(bottom, n * n - i + 1);
+            gridConnection.union(BOTTOM, n * n - i + 1);
         }
     }
 
     // open site (row, col) if it is not open already
     public void open(int row, int col) {
-        grid[row - 1][col - 1] = true;
+        row = row - 1;
+        col = col - 1;
+        grid[row][col] = true;
+        connectNeighbors(row, col);
     }
 
     // get corresponding number to connect in gridConnection
@@ -39,54 +42,39 @@ public class Percolation {
 
     // connect neighbors of element if they are opened
     private void connectNeighbors(int row, int col) {
-        if (isLeftBorder(col)) {
-
+        int current = getCorrespondingNumber(row, col);
+        if (isTopSideOpen(row, col)) {
+            int top = getCorrespondingNumber(row - 1, col);
+            gridConnection.union(current, top);
         }
-
-    }
-
-    private boolean isLeftBorder(int col) {
-        if (col == 0) {
-            return true;
+        if (isBottomSideOpen(row, col)) {
+            int bottom = getCorrespondingNumber(row + 1, col);
+            gridConnection.union(current, bottom);
         }
-        return false;
-    }
-
-    private boolean isRightBorder(int col) {
-        if (col == N) {
-            return true;
+        if (isLeftSideOpen(row, col)) {
+            int left = getCorrespondingNumber(row, col - 1);
+            gridConnection.union(current, left);
         }
-        return false;
-    }
-
-    private boolean isTobBorder(int row) {
-        if (row == 0) {
-            return true;
+        if (isRightSideOpen(row, col)) {
+            int right = getCorrespondingNumber(row, col + 1);
+            gridConnection.union(current, right);
         }
-        return false;
     }
 
-    private boolean isBottomBorder(int row) {
-        if (row == N) {
-            return true;
-        }
-        return false;
+    private boolean isTopSideOpen(int row, int col) {
+        return isOpen(row - 1, col);
     }
 
-    private boolean isTopLeftCorner(){
-        return false;
+    private boolean isBottomSideOpen(int row, int col) {
+        return isOpen(row + 1, col);
     }
 
-    private boolean isTopRightCorner(){
-        return false;
+    private boolean isLeftSideOpen(int row, int col) {
+        return isOpen(row, col - 1);
     }
 
-    private boolean isBottomLeftCorner(){
-        return false;
-    }
-
-    private boolean isBottomRightCorner(){
-        return false;
+    private boolean isRightSideOpen(int row, int col) {
+        return isOpen(row, col + 1);
     }
 
     // is site (row, col) open?
@@ -96,7 +84,7 @@ public class Percolation {
 
     // is site (row, col) full?
     public boolean isFull(int row, int col) {
-        return gridConnection.connected(top, getCorrespondingNumber(row, col));
+        return gridConnection.connected(TOP, getCorrespondingNumber(row, col));
     }
 
     // number of open sites
@@ -106,11 +94,13 @@ public class Percolation {
 
     // does the system percolate?
     public boolean percolates() {
-        return gridConnection.connected(top, bottom);
+        return gridConnection.connected(TOP, BOTTOM);
     }
 
     public static void main(String[] args) {
         Percolation p = new Percolation(5);
+        p.open(1, 1);
+        p.open(2, 1);
         System.out.print("SSSSS");
     }
 }
